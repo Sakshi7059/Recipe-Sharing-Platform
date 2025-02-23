293f65ff53e24101f136820138be41b9ef6a1f67
Sakshi Bhagwat <118345694+Sakshi7059@users.noreply.github.com>
2025-02-23 12:33:05 +0530

    Add files via upload

---
+++ src/pages/Home.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+     <img src="/8.jpg" class="img-fluid w-100 pt-1" alt="..."/ >
+     <div class="row row-cols-1 row-cols-md-2 g-4 m-2">
+  <div class="col">
+    <div class="card">
+      <img src="/donut.jpeg" class="card-img-top" alt="..."/>
+      <div class="card-body">
+        <h2 class="card-title">DONUTS</h2>
+        <p class="card-text" > Donuts that make every day a little sweeter.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="/6.jpg" class="card-img-top" alt="..."/ >
+      <div class="card-body">
+        <h2 class="card-title">MISSAL</h2>
+        <p class="card-text">Spicy, tangy, and packed with flavor! Misal pav is a Maharashtrian delight.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="/pizza.png" class="card-img-top" alt="..."/ >
+      <div class="card-body">
+        <h2 class="card-title">PIZZA</h2>
+        <p class="card-text">Slice by slice, we conquer the world.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="/12.jpg" class="card-img-top" alt="..." />
+      <div class="card-body">
+        <h2 class="card-title">SHAKES</h2>
+        <p class="card-text"> A taste worth remembering.</p>
+      </div>
+    </div>
+  </div>
+</div>
+    </div>
+  )
+}
+
+export default Home
